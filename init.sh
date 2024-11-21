b9eca9d6c2669e0948085cb8e5f1fbce7d819dfd
Monique Rio <mrio@umich.edu>
2024-11-21 09:46:00 -0500

    Dockerfile poetry in dev pip in production
    * Changes Docker structure so that there is poetry in the development
      step, and pip in the production step
    * Cleands up the documentation in the dockerfile
    * Adds some directories and filenames to .gitignore and .dockerignore
    * init.sh has shellcheck improvements and installs python packages

--- init.sh
+++ init.sh
@@ -5,12 +5,15 @@ if [ -f ".env" ]; then
 else
   echo "🌎 .env does not exist. Copying .env-example to .env"
   cp env.example .env
-  YOUR_UID=`id -u`
-  YOUR_GID=`id -g`
-  echo "🙂 Setting your UID ($YOUR_UID) and GID ($YOUR_UID) in .env"
-  docker run --rm -v ./.env:/.env alpine echo "$(sed s/YOUR_UID/$YOUR_UID/ .env)" > .env
-  docker run --rm -v ./.env:/.env alpine echo "$(sed s/YOUR_GID/$YOUR_GID/ .env)" > .env
+  YOUR_UID=$(id -u)
+  YOUR_GID=$(id -g)
+  echo "🙂 Setting your UID (${YOUR_UID}) and GID (${YOUR_UID}) in .env"
+  docker run --rm -v ./.env:/.env alpine echo "$(sed s/YOUR_UID/${YOUR_UID}/ .env)" >.env
+  docker run --rm -v ./.env:/.env alpine echo "$(sed s/YOUR_GID/${YOUR_GID}/ .env)" >.env
 fi
 
 echo "🚢 Build docker images"
 docker compose build
+
+echo "📦 Build python packages"
+docker compose run --rm app poetry install
